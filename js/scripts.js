3bf5ba5976b04b00a4281b05af8f9367fe1317f2
rdv0 <rdv0@ya.ru>
2019-03-30 12:30:22 +0300

    1
    1

--- js/scripts.js
+++ js/scripts.js
@@ -45,12 +45,12 @@
             id: 'h4-' +i,
         }).appendTo('#block-' +i);
         //а можно цену принять
-        //39 минтута
-        var price = prompt('Введите цену', '0.00');
+        //39 минута
+       // var price = prompt('Введите цену', '0.00');
 
         $('<span>', {
-          // html: i + '000 ',
-            html: price,
+           html: i + '000 ',
+            //html: price,
         }).appendTo('#h4-' +i);
 
         $('<i>', {
@@ -63,7 +63,7 @@
             click: function () {
                 //1. Прочитать текущую сумму в корзине
              // alert( $('#cart-total-sum').find('span').html() );
-              var totalSum = $('#cart-total-sum').find('span').html();
+              var totalSum = $('#cart-total-sum').find('span').html();//если нет аргумента - прочитать, если есть аргумент - записать его
                 //2. Прочитать текущее количество в вкорзине
                  var count = $('#cart-total-count').find('span').html();
 
@@ -71,7 +71,7 @@
                 var thisSum = $(this.parentNode).find('h4 span').html();
                 //alert (thisSum);
 
-                //4. Добавить к текущей сумме текущую цену (использовать приведение типов к числу или Number, или parceInt, или +. Каждый проход увеличивает totalSum на сумму товара из переменной thisSum
+                //4. Добавить к тотальной сумме текущую цену один раз (использовать приведение типов к числу или Number, или parceInt, или +. Каждый проход увеличивает totalSum на сумму товара из переменной thisSum
                 totalSum = Number(totalSum) + Number(thisSum);
 
                 //5. Добавить к текущему количеству 1 (единицу). Потому что за один клик добавляется только что-то одно товар
@@ -84,5 +84,32 @@
             },
         }).appendTo('#block-' +i);
     }
-	
-	
+
+	//просто интересная функция к теме обсуждения for
+    $('<div/>', {
+        id: 'foo',
+        html: r,
+    }).appendTo('#block-'+i);
+
+    var r = function () {
+	var k, n=6, rnd, color;
+    for(k=1; k<=n; k++){
+        rnd = Math.random();
+        if(rnd<0.2){
+            color = function () {
+                document.write("Красный<br/>");
+            }
+        }else{
+            if(rnd<0.5){
+                color = function () {
+                    document.write("Желтый<br/>");
+                }
+            }else{
+                color = function () {
+                    document.write("Зеленый<br/>");
+                }
+            }
+        }
+        color();
+    }
+    }
